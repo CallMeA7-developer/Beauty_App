ecbeb670a176c3576720c277592c78c0cf7c4119
CallMeA7-developer <aradiyas18@gmail.com>
2026-04-10 16:36:53 +0400

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import './index.css'
-import './i18n'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
