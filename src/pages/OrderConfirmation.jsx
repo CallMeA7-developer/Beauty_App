118102ce4b0a8dc33481ff0a006edcc250a93689
CallMeA7-developer <aradiyas18@gmail.com>
2026-04-10 12:36:24 +0400

    Updated OrderConfirmation.jsx

--- src/pages/OrderConfirmation.jsx
+++ src/pages/OrderConfirmation.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Link, useSearchParams } from 'react-router-dom'
+import { Link, useSearchParams, useNavigate } from 'react-router-dom'
 import {
   IoCheckmarkCircle,
   IoMailOutline,
